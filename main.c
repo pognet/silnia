8607f00565b4c4508266ef627c7b4bea2da74ffb
pognet <pogo@interbit.pl>
2018-10-25 12:38:00 +0200

    program silnia

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i=1;
+    int n;
+    printf("Program liczy n!. \n")
+    printf("Podaj liczbę n: %i", n);
+    scanf(%i, n)
+
+    for (i = 1, n <= 15; i<=n; ++i)
+    return 0;
+}
