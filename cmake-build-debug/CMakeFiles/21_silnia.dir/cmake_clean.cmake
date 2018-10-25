7985bf301b0cd348e20a5b45e0bdbed02458275a
pognet <pogo@interbit.pl>
2018-10-25 14:17:20 +0200

    poprawiony

---
+++ cmake-build-debug/CMakeFiles/21_silnia.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/21_silnia.dir/main.c.o"
+  "21_silnia.pdb"
+  "21_silnia"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/21_silnia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
-int main() {
-    int i=1;
-    int n;
-    printf("Program liczy n!. \n")
-    printf("Podaj liczbę n: %i", n);
-    scanf(%i, n)
+int licz_silnie(int podstawa) {
+    if (podstawa == 0) {
+        return 1;
+    } else {
+        int licznik;
+        int silnia=1;
 
-    for (i = 1, n <= 15; i<=n; ++i)
+        for (licznik = 1; licznik <= podstawa; ++licznik) {
+            silnia *= licznik;
+        }
+        return silnia;
+    }
+}
+
+int main() {
+   int podstawa;
+   int wynik;
+
+   printf("Podaj podstawę silni w przedziale 0 do 15: \n");
+   scanf("%d", &podstawa);
+
+   if(podstawa >= 0 && podstawa <= 15){
+       wynik = licz_silnie(podstawa);
+       printf("silnia : %d", wynik);
+
+   }
+   else {
+       printf("Podałeś liczbę z poza zakresu! \n");
+   }
     return 0;
 }
